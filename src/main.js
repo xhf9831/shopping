a7d6ffd2ed22cd35c4121b5a3ab4ec43875a8fb1
moban <18215615808@163.com>
2019-12-28 15:36:17 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import service from "./http"
+import Vant from 'vant';
+import 'vant/lib/index.css'
+import BScroll from 'better-scroll'
+import FastClick from 'fastclick'
 
+FastClick.attach(document.body);
+var BScroll = require('better-scroll')
+Vue.use(Vant);
+Vue.prototype.$axios = service
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/category',
+        name: 'category',
+        component: () =>
+            import ('../views/category/Category.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
